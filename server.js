641736984f9062ff448aa935fd47a928f50219bc
eliavm <eliavmaman2@gmail.com>
2018-07-29 19:00:05 +0300

    dfsdf

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-const cors=require('cors');
+const express = require('express');        // call express
+const app = express();                 // define our app using express
+const http = require('http');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+const cors = require('cors');
 
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 //mongoose.createConnection( 'mongodb://localhost/yarderstore' );
 mongoose.connect('mongodb://localhost:27017/yarderstore');
@@ -17,10 +21,10 @@ var port = process.env.PORT || 3000;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 const router = require('./router');            // get an instance of the express Router
-
+var numOusers = 0;
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'});
 });
 
 // more routes for our API will happen here
@@ -28,7 +32,29 @@ router.get('/', function(req, res) {
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+//
+// io.sockets.on('connection', function (socket) {
+//     console.log('Socket connected');
+//     // Socket event for gist created
+//     socket.on('gistSaved', function (gistSaved) {
+//         io.emit('gistSaved', gistSaved);
+//     });
+//
+//     // Socket event for gist updated
+//     socket.on('gistUpdated', function (gistUpdated) {
+//         io.emit('gistUpdated', gistUpdated);
+//     });
+// });
+io.on('connection', function (socket) {
 
+
+    socket.on('userLoggedin', function () {
+        numOusers++;
+        console.log('numOusers ' + numOusers);
+        io.emit('userLoggedin', numOusers);
+    });
+
+});
 mongoose.Promise = global.Promise;
 
 
@@ -55,6 +81,6 @@ db.once('open', function () {
 
 
 });
-app.listen(port, ()=>{
+server.listen(port, () => {
     console.log('Server listen on port 3000\n\n');
 });
